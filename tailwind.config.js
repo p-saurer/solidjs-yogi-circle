b7d449e9f55aacfcf83474e5b14ccef3fe4799e0
p-saurer <philipp.saurer1988@gmail.com>
2023-01-29 17:43:24 +0100

    Add logo, theme colors, upgrade packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "indigo-dye": "#153B50",
+      "green-yellow-crayola": "#F7EF99",
+      "morning-blue": "#81A034",
+      "deep-taupe": "#775B59",
+      "dark-sienna": "#32161F",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
