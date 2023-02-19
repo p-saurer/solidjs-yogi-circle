ee5a2f011e5e9122298352630d9123806ec9b9fc
p-saurer <philipp.saurer1988@gmail.com>
2023-02-19 16:01:48 +0100

    Add prettier setup, change tailwind setup a bit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      "indigo-dye": "#153B50",
-      "green-yellow-crayola": "#F7EF99",
-      "morning-blue": "#81A034",
-      "deep-taupe": "#775B59",
-      "dark-sienna": "#32161F",
+    extend: {
+      colors: {
+        "indigo-dye": "#153B50",
+        "green-yellow-crayola": "#F7EF99",
+        "morning-blue": "#81A034",
+        "deep-taupe": "#775B59",
+        "dark-sienna": "#32161F",
+        "as-black": "#171717",
+        "as-white": "#FAFAFA",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
